aaf26c05f85a7742d1dd21d0b565851fb93565c2
Connor Bren <wildweegee101@gmail.com>
2024-08-04 02:03:01 -0600

    click, followa, handycam, mapmanager autogenerated

---
+++ library/mapmanager.lua
@@ -0,0 +1,20 @@
+---@meta
+
+local mapmanager = {}
+
+---
+---@param x unknown?
+---@param y unknown?
+---@return unknown
+function mapmanager.getLevelAt(x, y) end
+
+---
+---@param x1 unknown?
+---@param y1 unknown?
+---@param x2 unknown?
+---@param y2 unknown?
+---@param ids unknown?
+---@return table
+function mapmanager.getLevelsIntersecting(x1, y1, x2, y2, ids) end
+
+return mapmanager
