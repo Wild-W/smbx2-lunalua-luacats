d74c8903ffc90c4023edd75c2c91e104cb884257
Connor Bren <wildweegee101@gmail.com>
2024-08-06 02:03:27 -0600

    mapmanager edits

--- library/mapmanager.lua
+++ library/mapmanager.lua
@@ -3,18 +3,18 @@
 local mapmanager = {}
 
 ---
----@param x unknown?
----@param y unknown?
----@return unknown
+---@param x number
+---@param y number
+---@return Level level World level
 function mapmanager.getLevelAt(x, y) end
 
 ---
----@param x1 unknown?
----@param y1 unknown?
----@param x2 unknown?
----@param y2 unknown?
----@param ids unknown?
----@return table
+---@param x1 number
+---@param y1 number
+---@param x2 number
+---@param y2 number
+---@param ids number[]|number
+---@return Level[] levels World levels
 function mapmanager.getLevelsIntersecting(x1, y1, x2, y2, ids) end
 
 return mapmanager
